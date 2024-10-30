1584f03339e7d5ca915cae2e86c4cc0076b7455b
Ginger Society Admin <admin@gingersociety.org>
2024-10-30 15:37:56 +0530

    feat: added rocket models

---
+++ src/rocket_models.rs
@@ -0,0 +1,22 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize)]
+pub struct RealtimeMessage {
+    topic: String,
+    payload: String,
+}
+
+// Implement the `ToString` trait for the struct
+impl ToString for RealtimeMessage {
+    fn to_string(&self) -> String {
+        // Convert the struct to a JSON string using serde_json
+        serde_json::to_string(self).unwrap_or_else(|_| "Failed to serialize".to_string())
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, JsonSchema)]
+pub struct MessageResponse {
+    /// This is a message from the server.
+    pub message: String,
+}
