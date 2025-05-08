9e82f44ad69458f3c01ce1eec1b9245416ccb811
Ginger Society Admin <admin@gingersociety.org>
2025-05-08 08:46:42 +0530

    feat: added remote port and remote studio port in db config

--- src/lib.rs
+++ src/lib.rs
@@ -454,6 +454,8 @@ pub struct DatabaseConfig {
     pub id: Option<String>,
     pub name: String,
     pub port: String,
+    pub remote_port: Option<String>,
+    pub remote_studio_port: Option<String>,
     pub studio_port: Option<String>,
     #[serde(default = "default_links")]
     pub links: Vec<Link>,
